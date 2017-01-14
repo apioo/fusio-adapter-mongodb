e54aefacde73e2c8c6e14f018b108a0012037846
Christoph Kappestein <k42b3.x@gmail.com>
2017-01-14 10:36:47 +0100

    add connection test

---
+++ tests/Connection/MongoDBTest.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Adapter\Sql\Tests\Connection;
+
+use Fusio\Adapter\Mongodb\Connection\MongoDB;
+use MongoDB\Database;
+use Fusio\Engine\Form\Builder;
+use Fusio\Engine\Form\Container;
+use Fusio\Engine\Form\Element\Input;
+use Fusio\Engine\Parameters;
+use Fusio\Engine\Test\EngineTestCaseTrait;
+
+/**
+ * MongoDBTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class MongoDBTest extends \PHPUnit_Framework_TestCase
+{
+    use EngineTestCaseTrait;
+
+    public function testGetConnection()
+    {
+        /** @var MongoDB $connection */
+        $connection = $this->getConnectionFactory()->factory(MongoDB::class);
+
+        $config = new Parameters([
+            'url'      => 'mongodb://127.0.0.1',
+            'options'  => '',
+            'database' => 'test',
+        ]);
+
+        $mongo = $connection->getConnection($config);
+
+        $this->assertInstanceOf(Database::class, $mongo);
+    }
+
+    public function testConfigure()
+    {
+        $connection = $this->getConnectionFactory()->factory(MongoDB::class);
+        $builder    = new Builder();
+        $factory    = $this->getFormElementFactory();
+
+        $connection->configure($builder, $factory);
+
+        $this->assertInstanceOf(Container::class, $builder->getForm());
+
+        $elements = $builder->getForm()->getProperty('element');
+        $this->assertEquals(3, count($elements));
+        $this->assertInstanceOf(Input::class, $elements[0]);
+        $this->assertInstanceOf(Input::class, $elements[1]);
+        $this->assertInstanceOf(Input::class, $elements[2]);
+    }
+}
