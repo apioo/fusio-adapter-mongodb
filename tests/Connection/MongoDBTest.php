c99ab66d37d5ce65597a63eed08dfb0bb6c41af7
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-01-08 16:59:42 +0100

    fix test

--- tests/Connection/MongoDBTest.php
+++ tests/Connection/MongoDBTest.php
@@ -68,7 +68,7 @@ class MongoDBTest extends TestCase
 
         $this->assertInstanceOf(Container::class, $builder->getForm());
 
-        $elements = $builder->getForm()->getProperty('element');
+        $elements = $builder->getForm()->getElements();
         $this->assertEquals(3, count($elements));
         $this->assertInstanceOf(Input::class, $elements[0]);
         $this->assertInstanceOf(Input::class, $elements[1]);
